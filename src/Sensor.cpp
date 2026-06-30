45db2104d018d417fe04cf457d009e06fc33f978
Supreeth <supreeths2008@gmai.com>
2026-06-30 22:48:29 +0530

    1

--- src/Sensor.cpp
+++
@@ -1,8 +0,0 @@
-#include "Sensor.h"
-
-Sensor::Sensor(int trigPin, int echoPin, int maxDistance) : sonar(trigPin, echoPin, maxDistance) {
-}
-
-int Sensor::GetDistance() {
-  return sonar.ping_cm();
-}
